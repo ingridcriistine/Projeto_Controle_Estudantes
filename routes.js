4bbb2f533cfe5a9f02b03e256a7f59a0598974d6
ingridcriistine <ingridrochacristine@gmail.com>
2024-08-16 09:42:52 -0300

    updates

---
+++ routes.js
@@ -0,0 +1,11 @@
+// Iniciando Route do Express
+const express = require('express');
+const route = express.Router();
+
+// Importando os Controllers
+const home = require('./src/controllers/home');
+
+// Iniciando as rotas
+route.get('/', home.pagInicialGet);
+
+module.exports = route;
